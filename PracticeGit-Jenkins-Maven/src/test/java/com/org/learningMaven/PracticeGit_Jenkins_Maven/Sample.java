ba4a0195fd4bdd7259e79e78f46666ae5f04a0f9
tggupta <tggupta22@gmail.com>
2017-08-27 17:44:24 +0530

    1st commit

---
+++ PracticeGit-Jenkins-Maven/src/test/java/com/org/learningMaven/PracticeGit_Jenkins_Maven/Sample.java
@@ -0,0 +1,19 @@
+package com.org.learningMaven.PracticeGit_Jenkins_Maven;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Sample {
+		
+	WebDriver driver=null;
+	
+	@Test
+	public void getPractive(){
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\Tarun\\workspace\\PracticeGit-Jenkins-Maven\\src\\lib2\\chromedriver_new.exe");	
+	driver=new ChromeDriver();
+	
+	driver.get("http://www.paytm.com");
+	}
+
+}
