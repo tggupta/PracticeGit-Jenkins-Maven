af68c4a903a91e8224094c6d020fa4fe7a14c4e2
tggupta <tggupta22@gmail.com>
2017-08-31 00:19:20 +0530

    Moving Changes to Script from Local repo to Github

--- PracticeGit-Jenkins-Maven/src/test/java/com/org/learningMaven/PracticeGit_Jenkins_Maven/Sample.java
+++ PracticeGit-Jenkins-Maven/src/test/java/com/org/learningMaven/PracticeGit_Jenkins_Maven/Sample.java
@@ -9,11 +9,13 @@ public class Sample {
 	WebDriver driver=null;
 	
 	@Test
-	public void getPractive(){
+	public void getPractive() throws InterruptedException{
 	System.setProperty("webdriver.chrome.driver", "C:\\Users\\Tarun\\workspace\\PracticeGit-Jenkins-Maven\\src\\lib2\\chromedriver_new.exe");	
 	driver=new ChromeDriver();
 	
 	driver.get("http://www.paytm.com");
+	Thread.sleep(2000);
+	driver.quit();
 	}
 
 }
